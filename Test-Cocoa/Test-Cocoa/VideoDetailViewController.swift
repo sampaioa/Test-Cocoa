74bbee6c37c16bbdb64f3fb59fa53c62b3e1afa1
André Sampaio <andresampaionz@hotmail.com>
2018-10-23 21:33:12 +1300

    YouTube Video EP10

--- Test-Cocoa/Test-Cocoa/VideoDetailViewController.swift
+++ Test-Cocoa/Test-Cocoa/VideoDetailViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+// Andre Update 23/10/2018
+
 class VideoDetailViewController: UIViewController {
     //@IBOutlet weak var webView: WKWebView!
     @IBOutlet weak var titleLabel: UILabel!
