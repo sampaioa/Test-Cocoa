2c9d52ac91458f72481d7e22fddde23a85a4cd52
André Sampaio <andresampaionz@hotmail.com>
2018-11-06 20:57:50 +1300

    resize when rotate

--- Test-Cocoa/Test-Cocoa/VideoDetailViewController.swift
+++ Test-Cocoa/Test-Cocoa/VideoDetailViewController.swift
@@ -20,6 +20,7 @@ class VideoDetailViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     @IBOutlet weak var webViewHeightConstraint: NSLayoutConstraint!
     
@@ -31,19 +32,31 @@ class VideoDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //webView.scalesPageToFit = true
 
         // Do any additional setup after loading the view.
     }
+    
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation){
+        let width = self.view.frame.size.width
+        let height = width/320 * 180
+        self.webViewHeightConstraint.constant = height
+    
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
+        
         if let vid = self.selectedVideo{
             self.titleLabel.text = vid.videoTitle
             self.descriptionLabel.text = vid.videoDescription
+            
             
             let width = self.view.frame.size.width
             let height = width/320 * 180
             
             // Adjust the height of the webview constrain
             self.webViewHeightConstraint.constant = height
+            
             
             
             let videoEmbedString = "<html><head><style type=\"text/css\">body {background-color: transparent;color: white;}</style></head><body style=\"margin:0\"><iframe frameBorder=\"0\" height=\"" + String(Int(Float(height))) + "\" width=\"" + String(Int(Float(width))) + "\" src=\"http://www.youtube.com/embed/" + vid.videoID + "?showinfo=0&modestbranding=1&frameborder=0&rel=0\"></iframe></body></html>"
