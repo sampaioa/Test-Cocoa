d7fef61fec760eb03a0d551ab786259eb1a022e8
André Sampaio <andresampaionz@hotmail.com>
2018-12-12 23:28:13 +1300

    new login screen

--- Test-Cocoa/Test-Cocoa/popupVC.swift
+++ Test-Cocoa/Test-Cocoa/popupVC.swift
@@ -1,9 +1,8 @@
 //
 //  popupVC.swift
-//  Test-Cocoa
+//  
 //
 //  Created by Andre Sampaio on 12/12/18.
-//  Copyright © 2018 Andre Sampaio. All rights reserved.
 //
 
-import Foundation
+import UIKit
